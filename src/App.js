717f6be5688aaf58befb2c3b13402991688ad8bc
vuongvgc <vuongvgc@gmail.com>
2021-05-12 22:34:42 +0700

    nabar: button toggle, sass, context

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Resume from "./pages/Resume";
+
 import Navbar from "./components/navbar-components/Navbar";
 import Footer from "./components/footer-components/Footer";
+
+import englishText from "./data/EnglishText";
+import vietnamText from "./data/VietnamText";
+import Context from "./context";
+
+import "./helpers/fontAwesome";
+import "./styles/main.scss";
+
 export default function App() {
+  const DEFAULT_TEXT_CONTENT = englishText;
+  const DEFAULT_LANGUAGE_TOGGLE = false;
+  const [textContent, setTextContent] = useState(DEFAULT_TEXT_CONTENT);
+  const [languageToggle, setLanguageToggle] = useState(DEFAULT_LANGUAGE_TOGGLE);
+  function handleSwitch() {
+    languageToggle === false
+      ? setTextContent(vietnamText)
+      : setTextContent(englishText);
+    setLanguageToggle(!languageToggle);
+  }
   return (
     <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/portfolio" component={Portfolio} />
-        <Route path="/resume" component={Resume} />
-      </Switch>
-      <Footer />
+      <Context.Provider value={{ textContent, FontAwesomeIcon, handleSwitch }}>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/resume" component={Resume} />
+        </Switch>
+        <Footer />
+      </Context.Provider>
     </BrowserRouter>
   );
 }
---
+++ src/components/common-component/SocialMediaIcons.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function SocialMediaIcons() {
+  const socialMediaIconsInfo = [
+    {
+      href: "mailto:konstantin.krumin@gmail.com",
+      extraClass: "",
+      iconPrefix: "far",
+      iconName: "envelope",
+    },
+    {
+      href: "https://www.linkedin.com/in/konstantinkrumin/",
+      extraClass: "",
+      iconPrefix: "fab",
+      iconName: "linkedin",
+    },
+    {
+      href: "https://twitter.com/KonstantinIK1",
+      extraClass: "",
+      iconPrefix: "fab",
+      iconName: "twitter",
+    },
+    {
+      href: "https://github.com/konstantinkrumin",
+      extraClass: "",
+      iconPrefix: "fab",
+      iconName: "github",
+    },
+    {
+      href: "https://codepen.io/konstantinkrumin",
+      extraClass: "hide-sm-screen",
+      iconPrefix: "fab",
+      iconName: "codepen",
+    },
+    {
+      href: "https://www.freecodecamp.org/konstantin.krumin",
+      extraClass: "hide-sm-screen",
+      iconPrefix: "fab",
+      iconName: "free-code-camp",
+    },
+  ];
+  return <div></div>;
+}
