2182ba70d1338fa4be260b42e32a9676fd9ee5f1
vuongvgc <vuongvgc@gmail.com>
2021-05-18 21:09:26 +0700

    resume: fix router

--- src/components/navbar-components/Navigation.js
+++ src/components/navbar-components/Navigation.js
@@ -1,14 +1,29 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Context from "../../context";
 import NavLink from "./NavLink";
 export default function Navigation() {
-  const { textContent } = useContext(Context);
+  const { textContent, FontAwesomeIcon } = useContext(Context);
+  const [isOpen, setIsOpen] = useState(false);
+  function handleClick() {
+    setIsOpen(!isOpen);
+  }
   return (
     <span>
       <span>
         <NavLink title={textContent.navigation.home} link="/" />
         <NavLink title={textContent.navigation.portfolio} link="/portfolio" />
         <NavLink title={textContent.navigation.resume} link="/resume" />
+      </span>
+      <span className="nav-small-screen">
+        {isOpen ? (
+          <span onClick={() => handleClick()}>
+            <FontAwesomeIcon icon={["fas", "times"]} />
+          </span>
+        ) : (
+          <span onClick={() => handleClick()}>
+            <FontAwesomeIcon icon={["fas", "bars"]} />
+          </span>
+        )}
       </span>
     </span>
   );
