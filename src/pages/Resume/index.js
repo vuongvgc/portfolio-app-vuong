ed1257b7d0be19c2db9500990cf458bcb00fe385
vuongvgc <vuongvgc@gmail.com>
2021-05-19 21:25:40 +0700

    fix deloy: resume section v3.1

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-// import ResumeTextSection from "../../components/resume-components/ResumeTextSection";
+import ResumeTextSection from "../../components/resume-components/ResumeTextSection";
 
 export default class Resume extends Component {
   render() {
     return (
       <div className="resume-section">
-        {/* <ResumeTextSection /> */}
-        <h1>Deploy Section</h1>
+        <ResumeTextSection />
       </div>
     );
   }
