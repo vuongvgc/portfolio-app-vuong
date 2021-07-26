d3aa8f2847d0d520e965aa45d64ab7eaefb66842
vuongvgc <vuongvgc@gmail.com>
2021-07-27 06:53:28 +0700

    change style porfolio resum

--- src/components/portfolio-components/PortfolioTilesContainer.js
+++ src/components/portfolio-components/PortfolioTilesContainer.js
@@ -3,11 +3,9 @@ import Context from "../../context";
 import { useContext } from "react";
 export default function PortfolioTilesContainer() {
   const { textContent } = useContext(Context);
-  console.log(textContent.portfolio);
   return (
     <div className="portfolio-tiles-container">
       {textContent.portfolio.map((item, index) => {
-        console.log(item);
         return (
           <ProjectTile
             key={index}
